3fdaea8909d0606fd9c5a9c136c2bc422825529a
camot0328 <camot0328@gmail.com>
2025-06-26 15:59:49 +0900

    version 0.2

--- src/App_Movie.jsx
+++ src/App_Movie.jsx
@@ -7,7 +7,7 @@ function App() {
   const getMovies = async () => {
     // localhost:8080 은 나중에 ALB 생성 후 DNS 로 변경 예정
     // const response = await fetch("http://localhost:8080")
-    const response = await fetch("/api")
+    const response = await fetch("https://camot.kro.kr/")
     const json = await response.json();
     setMovies(json.movies);
     setIsLoading(false);
